532c4471fa0db6d7a0a7f84957ee72ec9f5297f9
Dylan <britzdylan@gmail.com>
2024-04-06 01:09:15 +0200

    Update global.d.ts to remove unused JSX.HtmlTag properties

--- global.d.ts
+++ global.d.ts
@@ -1,20 +1,5 @@
 declare global {
   namespace JSX {
-    interface HtmlTag {
-      ['x-on:click']: string
-      ['x-data']: string
-      ['x-text']: string
-      ['x-model']: string
-      ['x-bind:']: string
-      ['x-show']: string
-      ['x-if']: string
-      ['x-for']: string
-      ['x-transition']: string
-      ['x-ref']: string
-      ['x-spread']: string
-      ['x-cloak']: string
-      ['x-on:keydown.space.prevent']: string
-      ['x-on:keyup.space.prevent']: string
-    }
+    interface HtmlTag {}
   }
 }
