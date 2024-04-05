78f4f8644e94ab3dc8d411120a67ae8928a6b1f9
Dylan <britzdylan@gmail.com>
2024-04-05 21:36:15 +0200

    Update HTML lang attribute in default layout

--- fragments/layouts/default.tsx
+++ fragments/layouts/default.tsx
@@ -3,7 +3,7 @@ import { viteAssets, viteReactRefresh } from 'adonisjsx'
 
 export function DefaultLayout({ children }: PropsWithChildren) {
   return (
-    <html id="page" class="dark" lang="en">
+    <html id="page" lang="en">
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
