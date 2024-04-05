9bf153194bbac936ad3cb8eb8cb2266a2a99400a
Dylan <britzdylan@gmail.com>
2024-04-05 19:23:37 +0200

    Update package name to hyper-flow and add @alpinejs/collapse dependency

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
+
 import htmx from 'htmx.org'
 
 window.htmx = htmx
 window.Alpine = Alpine
 
+Alpine.plugin(collapse)
 Alpine.start()
 
 console.log('Log from JS entrypoint')
