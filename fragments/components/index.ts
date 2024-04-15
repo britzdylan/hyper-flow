fd3a7323ed61a27e794dfafd6cb82e9e840715da
Dylan <britzdylan@gmail.com>
2024-04-15 13:27:20 +0200

    Refactor component imports in fragments/components/index.ts

--- fragments/components/index.ts
+++ fragments/components/index.ts
@@ -1,63 +1,8 @@
-import { Button } from '#primitives/button'
-import { Switch } from '#primitives/switch'
-import {
-  AccordionRoot,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
-} from '#primitives/accordion'
-import { Alert, AlertDescription, AlertTitle } from '#primitives/alert'
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '#primitives/alert-dialog'
-import { Avatar, AvatarFallback, AvatarImage } from '#primitives/avatar'
-import { Badge } from '#primitives/badge'
-import {
-  Breadcrumb,
-  BreadcrumbEllipsis,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '#primitives/breadcrumbs'
-
-export {
-  Button,
-  Switch,
-  AccordionRoot,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
-  Alert,
-  AlertDescription,
-  AlertTitle,
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-  Badge,
-  Breadcrumb,
-  BreadcrumbEllipsis,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-}
+export * from '#primitives/button'
+export * from '#primitives/switch'
+export * from '#primitives/accordion'
+export * from '#primitives/alert'
+export * from '#primitives/alert-dialog'
+export * from '#primitives/avatar'
+export * from '#primitives/badge'
+export * from '#primitives/breadcrumbs'
