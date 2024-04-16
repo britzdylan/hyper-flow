c95ed954046a477b615bb2ab2b32cdaf87133a85
Dylan <britzdylan@gmail.com>
2024-04-16 10:06:45 +0200

    Add @alpinejs/focus package and update imports in fragments/components/index.ts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,13 @@
 import Alpine from 'alpinejs'
 import collapse from '@alpinejs/collapse'
+import focus from '@alpinejs/focus'
 
 import htmx from 'htmx.org'
 
 window.htmx = htmx
 window.Alpine = Alpine
 
+Alpine.plugin(focus)
 Alpine.plugin(collapse)
 Alpine.start()
 
