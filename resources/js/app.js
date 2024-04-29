415133b75d98b2916b690e6eceb2ddb43030d8fa
Dylan <britzdylan@gmail.com>
2024-04-29 18:04:40 +0200

    Refactor app.js by removing unused code and dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ import collapse from '@alpinejs/collapse'
 import focus from '@alpinejs/focus'
 import anchor from '@alpinejs/anchor'
 import '../css/app.css'
-import { radioGroup, tabsData } from '../../fragments/components/index'
 
 import htmx from 'htmx.org'
 
@@ -13,50 +12,6 @@ window.Alpine = Alpine
 Alpine.plugin(focus)
 Alpine.plugin(collapse)
 Alpine.plugin(anchor)
-
-const progress = (initValue = 0, endValue = 100, duration = 500) => ({
-  init() {
-    console.log(initValue, 'initValue')
-    this.value = initValue
-
-    this.timer = setTimeout(() => (this.value = endValue), duration)
-  },
-  timer: null,
-  value: 0,
-  get progressBar() {
-    return `transform: translateX(-${100 - this.value}%);`
-  },
-  destroy() {
-    clearTimeout(this.timer)
-  },
-})
-
-const progressInterval = (initValue = 0, endValue = 100, delay = 500, step = 10) => ({
-  init() {
-    console.log(initValue, 'initValue')
-    this.value = initValue
-
-    this.timer = setInterval(() => {
-      this.value += step
-      if (this.value >= endValue) {
-        clearInterval(this.timer)
-      }
-    }, delay)
-  },
-  timer: null,
-  value: 0,
-  get progressBar() {
-    return `transform: translateX(-${100 - this.value}%);`
-  },
-  destroy() {
-    clearInterval(this.timer)
-  },
-})
-
-Alpine.data('progress', progress)
-Alpine.data('progressInterval', progressInterval)
-Alpine.data('radioGroup', radioGroup)
-Alpine.data('tabsData', tabsData)
 
 Alpine.start()
 
