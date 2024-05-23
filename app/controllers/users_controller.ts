755918f24d2d204d8fc84e8bd9d13df3c0a369c4
Dylan <britzdylan@gmail.com>
2024-05-23 20:20:01 +0200

    Layout testing

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,5 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Home from '#pages/home'
+import { UserDashboardLayout } from '#layouts/dashboard'
 
 export default class UsersController {
   /**
@@ -8,7 +9,10 @@ export default class UsersController {
   async index({ jsx }: HttpContext) {
     const start = performance.now()
 
-    const returnValue = await jsx(Home)
+    const returnValue = await jsx(Home, {
+      data: {},
+      layout: UserDashboardLayout,
+    })
     const end = performance.now()
     const used = process.memoryUsage().heapUsed / 1024 / 1024
 
