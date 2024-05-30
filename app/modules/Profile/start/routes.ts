417fe757b47feb928af80daaf13f8bbbe94af369
Dylan <britzdylan@gmail.com>
2024-05-30 17:15:02 +0200

    profile routes

---
+++ app/modules/Profile/start/routes.ts
@@ -0,0 +1,22 @@
+import router from '@adonisjs/core/services/router'
+import ProfileController from '#modules/Profile/app/controllers/profile'
+import { ProfileConfig } from '#modules/config'
+const { renderProfilePage, patchProfile, putProfileImage, deleteProfileImage } =
+  ProfileConfig.actions
+const { routeIdPrefix } = ProfileConfig
+
+router
+  .get(`${renderProfilePage.route}`, [ProfileController, 'renderProfilePage'])
+  .as(`${routeIdPrefix}renderProfilePage`)
+
+router
+  .patch(`${patchProfile.route}`, [ProfileController, 'patchProfile'])
+  .as(`${routeIdPrefix}patchProfile`)
+
+// router
+//   .put(`${putProfileImage.route}`, [ProfileController, 'putProfileImage'])
+//   .as(`${routeIdPrefix}putProfileImage`)
+
+// router
+//   .delete(`${deleteProfileImage.route}`, [ProfileController, 'deleteProfileImage'])
+//   .as(`${routeIdPrefix}deleteProfileImage`)
