54e33ff05220d1f8c64128d78bbce73985274d7e
Dylan <britzdylan@gmail.com>
2024-05-30 17:16:36 +0200

    base dashboard

---
+++ UI/components/base/dashboard.tsx
@@ -0,0 +1,22 @@
+import { Button } from '#primitives/button'
+import { JsxElementProps } from '#ui/lib/types'
+import { PropsWithChildren } from 'adonisjsx'
+
+export function DashboardSettingsBase({ children, ...props }: PropsWithChildren<JsxElementProps>) {
+  return (
+    <section class="w-full max-w-screen-xl mx-auto py-12 px-6 md:px-0 flex gap-12" {...props}>
+      <aside class="min-w-[250px]">
+        <ul class="flex flex-col p-4 text-small font-medium text-foreground/50">
+          <li>
+            <a href="/settings/general" title="Settings">
+              <Button class="w-full !justify-start !text-foreground" variant="ghost">
+                General
+              </Button>
+            </a>
+          </li>
+        </ul>
+      </aside>
+      <div class="flex-grow w-full">{children}</div>
+    </section>
+  )
+}
