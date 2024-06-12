f32c3be20940b70664de9197cb4ec71ed31b670f
Dylan <britzdylan@gmail.com>
2024-06-12 17:23:14 +0200

    Auth Mails

--- UI/pages/home.tsx
+++ UI/pages/home.tsx
@@ -1,7 +1,6 @@
 import type User from '#models/user'
 import { PublicFooter } from '#ui/components/project/shared/footer'
 import { PublicHeader } from '#ui/components/project/shared/header'
-import { ResetPasswordRequestEmail } from '#ui/emails/transactional'
 
 interface Home {
   user?: User
@@ -11,7 +10,6 @@ export default function Home({ user }: Home) {
     <div class="relative w-full p-4 md:p-0 flex-grow flex flex-col">
       <PublicHeader user={user} />
       <main class="w-full  flex-grow flex flex-col">
-        <ResetPasswordRequestEmail />
         <section class="flex-grow flex flex-col items-center justify-center">
           <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Hyper Flow</h1>
           <p class="leading-7 [&:not(:first-child)]:mt-6">
